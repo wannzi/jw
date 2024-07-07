5d6883245b6f521da93e9626f5ca7bc1611361f3
tuxiu21 <zhangyufan709@gmail.com>
2024-07-07 15:21:32 +0800

    complete rule main interface

--- src/views/RuleIndexView.vue
+++ src/views/RuleIndexView.vue
@@ -9,13 +9,16 @@
             <span class="rulebox-title">公共规则库</span>
           </div>
           <div class="rule-main-ctr">
+            <div class="bg-text">
+              <span>双击运行规则</span>
+            </div>
             <div class="rule-instance">
               <div class="rule-instance-icon">
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -23,8 +26,8 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -32,8 +35,8 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -41,8 +44,8 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -50,8 +53,8 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -59,8 +62,8 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -68,8 +71,8 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
             <div class="rule-instance">
@@ -77,10 +80,20 @@
                 <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
                 <span class="rule-instance-font">招投标违规</span>
               </div>
-              <div>
-                查看
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
               </div>
             </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon">
+                <img src="../assets/Rule/rule-ico1.svg" width="50" height="50">
+                <span class="rule-instance-font">招投标违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+              </div>
+            </div>
+            
            
           </div>
           
@@ -89,7 +102,118 @@
        
     </div>
     <div class="pri">
-      right
+      <div class="rule-ctr">
+          <div class="rule-head">
+            <img src="../assets/Rule/pri-folder.svg" width="50" height="50">
+            <span class="rulebox-title">私有规则库</span>
+          </div>
+          <div class="rule-main-ctr">
+            <div class="bg-text">
+              <span>双击运行规则</span>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon pri-color-instance">
+                <img src="../assets/Rule/pri-rule-ico2.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri">旧房改造违规</span>
+              </div>
+              <div class="rule-text-btn-ctr">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            <div class="rule-instance">
+              <div class="rule-instance-icon" style="background-color: transparent;border: 2px dashed white; cursor: pointer;">
+                <img src="../assets/Rule/plus.svg" width="50" height="50"
+                >
+                <span class="rule-instance-font rule-instance-font-pri"></span>
+              </div>
+              <div class="rule-text-btn-ctr" style="visibility: hidden;;">
+                <span class="rule-text-btn-font">查看</span>
+                <span class="rule-text-btn-font">编辑</span>
+                <span class="rule-text-btn-font">删除</span>
+              </div>
+            </div>
+            
+            
+           
+          </div>
+          
+
+       </div>
     </div>
   </div>
 </template>
@@ -135,7 +259,12 @@
 }
 .pri {
   flex: 1;
-  background-color: blue;
+  /* background-color: red; */
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 .rule-ctr{
@@ -179,6 +308,8 @@
 
   /* margin: 20px; */
   padding: 20px;
+
+  position: relative;
 }
 .rule-instance{
 
@@ -193,6 +324,8 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  z-index: 30;
 }
 .rule-instance-icon{
   display: flex;
@@ -202,13 +335,65 @@
   background-color: #88acdb;
   border-radius: 10px;
 
-  height: 100px;
-  width: 100px;
+  height: 110px;
+  width: 110px;
 
 }
+
+/* css定义位于后面  优先级更高*/
+.pri-color-instance{
+  background-color: #4095e5;
+}
+
 .rule-instance-font{
   font-family: youshe;
   color: #FFFFFF;
   font-size: 1vw;
+
+  white-space: nowrap;
 }
+
+
+.rule-instance-font-pri{
+  color: #fcca00;
+}
+
+.rule-instance:hover .rule-text-btn-ctr{
+  visibility: visible;
+}
+
+
+.rule-text-btn-ctr{
+  margin-top: 4px;
+  margin-bottom: 4px;
+
+  visibility: hidden;
+
+  display: flex;
+  flex-direction: row;
+  gap: 3px;
+}
+.rule-text-btn-font{
+  color:#8395a6;
+  cursor: pointer;
+}
+
+.rule-text-btn-font:hover{
+  font-weight: bold;
+  /* color: red */
+}
+
+.bg-text{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2rem;
+
+  z-index: 29;
+}
+.bg-text>span{
+  color: #456e92;
+}
+
 </style>
